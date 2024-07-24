4806118c7e87d197b2c27ac314b61bfa819064a1
IlchenkoRoman <ilchenko.roma00@yandex.ru>
2024-07-24 13:36:41 +0300

    Изменения для git pages

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { Task, initializeDatabase } = require('./db');
 
 const app = express();
 const PORT = 3000;
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, './')));
 
 initializeDatabase();
 
@@ -48,7 +48,7 @@ app.delete('/api/tasks/:id', async (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(PORT, () => {
